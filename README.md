bb8bd3777501ae5ff4b5380a4bedef3c82c42a3d
Marcin Matlaszek <mmatlaszek@gmail.com>
2015-02-08 15:36:31 +0100

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # gostream
-Simple server for streaming local files to remote clients via TCP.
+Simple server for streaming local files to remote clients via TCP. 
+
+
+### Installation:
+
+gostream depends on github.com/ActiveState/tail module
+
+```sh
+$ go get https://github.com/ActiveState/tail
+```
+
+Get gostream and build it!
+```sh
+$ git clone https://github.com/emate/gostream 
+$ go build gostream.go
+```
+
+### How to run:
+Run server
+```sh
+$ gostream [-l 0.0.0.0:8080] <filename>
+```
+
+On the other side, you can connect to socket and receive streamed file:
+```sh
+$ telnet REMOVE_GOSTREAM_SERVER_IP PORT
+```
+
---
+++ gostream.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"time"
+
+	"github.com/ActiveState/tail"
+)
+
+func ReadFile(filename string, stream chan<- string) {
+	seekinfo := tail.SeekInfo{Whence: os.SEEK_END}
+	t, _ := tail.TailFile(filename, tail.Config{Follow: true, Location: &seekinfo})
+	for line := range t.Lines {
+		stream <- line.Text + "\n"
+	}
+}
+
+func client(conn net.Conn, c <-chan string) {
+	for msg := range c {
+		conn.Write([]byte(msg))
+	}
+}
+func server(channels map[chan string]bool, input <-chan string) {
+	for {
+		fmt.Println("Servering!")
+		for msg := range input {
+			for k, _ := range channels {
+				fmt.Println("Sending")
+				k <- msg
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage %s <FILENAME>", os.Args[0])
+	flag.PrintDefaults()
+	os.Exit(1)
+}
+
+func main() {
+
+	flag.Usage = usage
+	address := flag.String("l", "localhost:8080", "Listen address")
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) < 1 {
+		fmt.Println("Filepath argument missing")
+		usage()
+	}
+	var filename string = args[0]
+
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		fmt.Printf("no such file : %s", filename)
+		usage()
+	}
+
+	connections := make(map[net.Conn]bool)
+	channels := make(map[chan string]bool)
+	filestream := make(chan string)
+
+	fmt.Println(*address)
+	l, err := net.Listen("tcp", *address)
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+	go ReadFile(filename, filestream)
+	go server(channels, filestream)
+	for {
+		c, _ := l.Accept()
+		defer c.Close()
+		channel := make(chan string)
+		channels[channel] = true
+		connections[c] = true
+		go client(c, channel)
+	}
+}
